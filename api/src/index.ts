948aab7cbc6dae0f528a69bcc80ae03af8b18f98
Forester04 <erickforester04@gmail.com>
2024-05-22 16:34:38 +0300

    Backend setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,9 +2,14 @@ import express from  'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 const { PORT } = process.env || 3000;
 
 const app = express();
+
+app.get('/', (req, res) => {
+    return res.json({ok: true});
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
