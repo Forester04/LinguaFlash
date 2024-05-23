0f6b7a14e0252cb21c023f2ed74bc93f2b603e18
Forester04 <erickforester04@gmail.com>
2024-05-23 14:58:08 +0300

    Fixing node-fetch error and Updating the api code to output the results

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,24 @@
 import express from  'express';
+import 'isomorphic-fetch'
 import dotenv from 'dotenv';
+import { getXataClient } from './xata';
+import fetch from 'isomorphic-fetch';
 dotenv.config();
 
 
 const { PORT } = process.env || 3000;
 
 const app = express();
+app.use(express.json({ limit: '50mb'}));
 
-app.get('/', (req, res) => {
-    return res.json({ok: true});
+const client = getXataClient(fetch);
+
+app.get('/', async (req, res) => {
+    const result = await client.db.sets.getAll();
+    return res.json({ results: result });
+    // return res.json({ ok: true});
 });
+
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
